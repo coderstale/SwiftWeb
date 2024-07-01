acd326196c4f55942cbb6651545e0d94fd9c1202
coderstale <coderstale@gmail.com>
2024-07-01 21:27:40 +0530

    commit now

--- cli.py
+++ cli.py
@@ -2,9 +2,11 @@ import click
 from my_framework.server import run as run_server
 from logging_config import logger
 
+
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 @click.option('--port', default=8000, help='Port to run the server on.')
@@ -12,5 +14,7 @@ def runserver(port):
     logger.info(f"Starting server on port {port}")
     run_server(port=port)
 
+
 if __name__ == '__main__':
     cli()
+
