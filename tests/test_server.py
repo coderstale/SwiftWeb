a17f595187f0f347db8ba539b453d479810ed5ec
coderstale <coderstale@gmail.com>
2024-07-01 21:34:56 +0530

    Fix flake8 issues

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,12 +2,14 @@ import pytest
 from http.server import HTTPServer
 from my_framework.server import RequestHandler
 
+
 @pytest.fixture
 def http_server():
-    server_address = ('', 8001)
+    server_address = ("", 8001)
     httpd = HTTPServer(server_address, RequestHandler)
     yield httpd
     httpd.server_close()
+
 
 def test_server_starts(http_server):
     assert http_server.server_address[1] == 8001
